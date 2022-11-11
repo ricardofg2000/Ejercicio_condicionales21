eb00a1d8c4ce418a6b27c3d263387f95168ac885
ricardofg2000 <ricardofg2000@gmail.com>
2022-11-11 07:58:57 +0100

    cambio en el enunciado

--- src/unico/Ejercicio21.java
+++ src/unico/Ejercicio21.java
@@ -8,7 +8,7 @@ public class Ejercicio21 {
 		/* 
 		Implementa el juego piedra, papel y tijera. Primero, 
 		el usuario 1 introduce su jugada y luego el usuario 2. 
-		Si alguno de los usuarios introduce una opción incorrecta, 
+		Si alguno de los usuarios introduce una opción incorrecta 
 		el programa deberá mostrar un mensaje de error.
 
 		Ejemplo 1:
